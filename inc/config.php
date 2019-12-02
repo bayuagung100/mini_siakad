2d3186abfecd22e97280c570cab6d9009cdec8d6
root <root@absensi.example.com>
2019-12-02 15:39:40 +0700

    dari server

--- inc/config.php
+++ inc/config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
-$database = "mini_siakad";
+$password = "root";
+$database = "admin_default";
  
 // Create connection
 $mysqli = new mysqli($servername, $username, $password, $database);
--- inc/helper.php
+++ inc/helper.php
@@ -2,8 +2,8 @@
 
 // kumpulan fungsi-fungsi yang sering digunakan
 function base_url($var){
-    // $url = sprintf("%s://%s%s", isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http', $_SERVER['SERVER_NAME'], $_SERVER['REQUEST_URI']);
-    $url = "http://localhost/mini_siakad/".$var;
+    //$url = sprintf("%s://%s%s", isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http', $_SERVER['SERVER_NAME'], $_SERVER['REQUEST_URI']);
+    $url = "http://183.91.66.138/".$var;
     return $url;
 }
 
